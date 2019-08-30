047c2e31a2c3cc94201b2c77a98fbf98cf1a7f6c
ChrisAaren <chrisaaren01@gmail.com>
2019-08-30 16:33:41 -0600

    Added extra characters to variable unwanted

--- wordCount.py
+++ wordCount.py
@@ -14,7 +14,7 @@ def wordCounter():
     words = contents.split()
     words = [i.strip('\n') for i in words]
     words = [i.replace('"', '') for i in words]
-    unwanted = ',.--:;'
+    unwanted = ',.--:;'''
     words = [i.lower() for i in words]
     dictionary = {}
     for i in words:
